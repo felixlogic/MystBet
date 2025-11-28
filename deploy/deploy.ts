333ff5efcdbf01c4d65caca00630f3c3f4bb8909
felixlogic <ubwospg020@outlook.com>
2025-11-28 15:23:38 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedMystBetGame = await deploy("MystBetGame", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`MystBetGame contract: `, deployedMystBetGame.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_mystBetGame"; // id required to prevent reexecution
+func.tags = ["MystBetGame"];
